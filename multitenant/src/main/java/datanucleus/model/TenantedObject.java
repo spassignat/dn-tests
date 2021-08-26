9d931af4e291aefe949b20ddf84839a03a4f5042
spassignat <passignat@hotmail.com>
2021-08-26 09:25:38 +0200

    add multitenant test on tenant-id

---
+++ multitenant/src/main/java/datanucleus/model/TenantedObject.java
@@ -0,0 +1,31 @@
+package datanucleus.model;
+
+
+import org.datanucleus.api.jpa.annotations.MultiTenant;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "TenantedObject")
+@MultiTenant(columnLength = 40)
+public class TenantedObject {
+	@Id
+	long id;
+	String name;
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
---
+++ multitenant/src/test/java/datanucleus/tests/EMFMultitenantTest.java
@@ -0,0 +1,166 @@
+/**********************************************************************
+ Copyright (c) 2006 Erik Bengtson and others. All rights reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ Contributors:
+ 2007 Andy Jefferson - rewritten to new test.framework/samples
+ ...
+ **********************************************************************/
+package datanucleus.tests;
+
+import datanucleus.model.TenantedObject;
+import org.datanucleus.PropertyNames;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for JPQL "SELECT" queries.
+ */
+public class EMFMultitenantTest {
+	private static boolean initialised = false;
+
+	@Test
+	public void testCreateQuery() throws IOException {
+		long id = System.currentTimeMillis();
+		Properties ps1 = getProperties();
+		ps1.put(PropertyNames.PROPERTY_MAPPING_TENANT_ID, "T1");
+		EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("JPATest", ps1);
+		EntityManager em1 = emf1.createEntityManager();
+		EntityTransaction tx1 = em1.getTransaction();
+		try {
+			tx1.begin();
+			TenantedObject p = new TenantedObject();
+			p.setId(id);
+			em1.persist(p);
+			tx1.commit();
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} finally {
+			if (tx1.isActive()) {
+				tx1.rollback();
+			}
+			em1.close();
+		}
+		em1 = emf1.createEntityManager();
+		tx1 = em1.getTransaction();
+		try {
+			tx1.begin();
+			TypedQuery<TenantedObject> query = em1.createQuery("SELECT t FROM TenantedObject t where t.id=:i", TenantedObject.class);
+			query.setParameter("i", id);
+			TenantedObject singleResult = query.getSingleResult();
+			assertNotNull(singleResult);
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} finally {
+			if (tx1.isActive()) {
+				tx1.rollback();
+			}
+			em1.close();
+		}
+		Properties ps2 = getProperties();
+		ps2.put(PropertyNames.PROPERTY_MAPPING_TENANT_ID, "T2");
+		EntityManagerFactory emf2 = Persistence.createEntityManagerFactory("JPATest", ps2);
+		EntityManager em2 = emf2.createEntityManager();
+		EntityTransaction tx2 = em2.getTransaction();
+		try {
+			tx2.begin();
+			TypedQuery<TenantedObject> query = em2.createQuery("SELECT t FROM TenantedObject t where t.id=:i", TenantedObject.class);
+			query.setParameter("i", id);
+			TenantedObject singleResult = query.getSingleResult();
+			assertNull(singleResult);
+		} catch (PersistenceException e) {
+			// expected, but would like to eventually have IllegalArgumentException
+		} finally {
+			if (tx2.isActive()) {
+				tx2.rollback();
+			}
+			em2.close();
+		}
+	}
+
+	private Properties getProperties() throws IOException {
+		String name = getClass().getName().replace(".", "/") + ".properties";
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream resourceAsStream = classLoader.getResourceAsStream(name);
+		Properties ps1 = new Properties();
+		ps1.load(resourceAsStream);
+		return ps1;
+	}
+
+	@Test
+	public void testCreateFind() throws IOException {
+		long id = System.currentTimeMillis();
+		Properties ps1 = getProperties();
+		ps1.put(PropertyNames.PROPERTY_MAPPING_TENANT_ID, "T1");
+		Properties ps2 = getProperties();
+		ps2.put(PropertyNames.PROPERTY_MAPPING_TENANT_ID, "T2");
+		EntityManagerFactory emf1 = Persistence.createEntityManagerFactory("JPATest", ps1);
+		EntityManager em1 = emf1.createEntityManager();
+		EntityTransaction tx1 = em1.getTransaction();
+		try {
+			tx1.begin();
+			TenantedObject p = new TenantedObject();
+			p.setName("TOTO " + id);
+			p.setId(id);
+			em1.persist(p);
+			tx1.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+			// expected, but would like to eventually have IllegalArgumentException
+		} finally {
+			if (tx1.isActive()) {
+				tx1.rollback();
+			}
+			em1.close();
+		}
+		em1 = emf1.createEntityManager();
+		tx1 = em1.getTransaction();
+		try {
+			tx1.begin();
+			TenantedObject tenantedObject = em1.find(TenantedObject.class, id);
+			assertNotNull(tenantedObject);
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} finally {
+			if (tx1.isActive()) {
+				tx1.rollback();
+			}
+			em1.close();
+		}
+		EntityManagerFactory emf2 = Persistence.createEntityManagerFactory("JPATest", ps2);
+		EntityManager em2 = emf2.createEntityManager();
+		EntityTransaction tx2 = em2.getTransaction();
+		try {
+			tx2.begin();
+			TenantedObject tenantedObject = em2.find(TenantedObject.class, id);
+			assertNull(tenantedObject);
+		} catch (PersistenceException e) {
+			// expected, but would like to eventually have IllegalArgumentException
+		} finally {
+			if (tx2.isActive()) {
+				tx2.rollback();
+			}
+			em2.close();
+		}
+	}
+}
